00c6955e61f38365b4538caab0ac64588dc9d1c0
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2021-01-16 13:07:08 -0500

    maryjane complete with CRUD

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -6,22 +6,35 @@ class Api::ProductsController < ApplicationController
 
   def create
     @product = Product.new(
-      {name: 'name', strain: 'strain', price_per_gram: ''}
+      name: params[:name],
+      strain: params[:strain],
+      price_per_gram: params[:price_per_gram],
+      mood: params[:mood],
+      thc_percentage: params[:thc_percentage],
+      image_url: params[:image_url]
     )
+    @product.save
     render 'show.json.jb'
   end
 
   def show
-    @product = Product.find_by(id: params['id'])
+    @product = Product.find_by(id: params[:id])
     render 'show.json.jb'
   end
 
   def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name] || @product.name
+    @product.strain = params[:strain] || @product.strain
+    @product.price_per_gram = params[:price_per_gram] || @product.price_per_gram
+    @product.mood = params[:mood] || @product.mood
+    @product.thc_percentage = params[:thc_percentage] || @product.thc_percentage
+    @product.image_url = params[:image_url] || @product.image_url
     render 'show.json.jb'
   end
 
   def destroy
-    product = Product.find_by(id: params['id'])
+    product = Product.find_by(id: params[:id])
     product.destroy
   end
 
